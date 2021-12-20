14c966bf381c71590a0e641152b29f1607624628
sarahwesley <92330391+sarahwesley@users.noreply.github.com>
2021-12-20 15:07:29 -0500

    request parameters added

--- server.js
+++ server.js
@@ -9,13 +9,13 @@ function filterByQuery(query, animalsArray) {
     let filteredResults = animalsArray;
     if (query.personalityTraits) {
         // Save personalityTraits as a dedicated array.
-        if (typeof query.personalityTraits === 'string') {
-          personalityTraitsArray = [query.personalityTraits];
-        } else {
-          personalityTraitsArray = query.personalityTraits;
-        }
+    if (typeof query.personalityTraits === 'string') {
+    personalityTraitsArray = [query.personalityTraits];
+    } else {
+    personalityTraitsArray = query.personalityTraits;
+    }
         // Loop through each trait in the personalityTraits array:
-        personalityTraitsArray.forEach(trait => {
+    personalityTraitsArray.forEach(trait => {
           // Check the trait against each animal in the filteredResults array.
           // Remember, it is initially a copy of the animalsArray,
           // but here we're updating it for each trait in the .forEach() loop.
@@ -23,30 +23,45 @@ function filterByQuery(query, animalsArray) {
           // array will then contain only the entries that contain the trait,
           // so at the end we'll have an array of animals that have every one 
           // of the traits when the .forEach() loop is finished.
-          filteredResults = filteredResults.filter(
-            animal => animal.personalityTraits.indexOf(trait) !== -1
-          );
+    filteredResults = filteredResults.filter(
+        animal => animal.personalityTraits.indexOf(trait) !== -1
+        );
         });
-      }
-      if (query.diet) {
+    }
+    if (query.diet) {
         filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-      }
-      if (query.species) {
+    }
+    if (query.species) {
         filteredResults = filteredResults.filter(animal => animal.species === query.species);
-      }
-      if (query.name) {
+    }
+    if (query.name) {
         filteredResults = filteredResults.filter(animal => animal.name === query.name);
-      }
+    }
       // If personalityTraits is a string, place it into a new array and save.
       // return the filtered results:
     return filteredResults;
 }
+
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+}
+
 app.get('/api/animals', (req,res) => {
     let results = animals;
     if (req.query) {
         results = filterByQuery(req.query, results);
     }
     res.json(results);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result) {
+        res.json(result);
+    } else {
+        res.send(404);
+    }
 });
 
 app.listen(PORT, () => {
